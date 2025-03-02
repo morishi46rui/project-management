f65b882459a6077e357960079c7da9687f187b43
宮森 瑠偉 <r.miyamori@eisansystem.jp>
2025-03-02 11:44:40 +0900

    Remove: フッター削除

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -8,7 +8,6 @@ import styles from './layout.module.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-// MUI のテーマを作成
 const theme = createTheme({
   palette: {
     primary: {
@@ -32,16 +31,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           <CssBaseline />
           <div className={styles.container}>
             {children}
-            <footer className={styles.footer}>
-              <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Powered by{' '}
-                <img src={staticPath.vercel_svg} alt="Vercel Logo" className={styles.logo} />
-              </a>
-            </footer>
           </div>
         </ThemeProvider>
       </body>
